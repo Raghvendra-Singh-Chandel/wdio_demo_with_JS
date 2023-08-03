67329ea6b95d22fbd4f08e9214c22922221b2e0f
= <=>
2023-08-03 17:17:22 +0530

    resolve chrome crashed error

--- wdio.conf.js
+++ wdio.conf.js
@@ -50,9 +50,13 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
-    capabilities: [{
-        browserName: 'chrome'
-    }],
+    capabilities: [
+        {
+            browserName: 'chrome',
+            'goog:chromeOptions': { 'args': ['--disable-gpu'] }
+        }
+    ]
+    
 
     //
     // ===================
